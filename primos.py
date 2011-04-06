c6ceaafea284b35f2fa758a4f19dde444e6d53e2
Diego Gurruchaga <diegog172@gmail.com>
2011-04-06 12:06:22 -0300

    Agrego primos.py

---
+++ primos.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf_8 -*-
+# primos.py: calculo de la sucesion
+from math import sqrt
+
+def prim(n):
+	"""Muestra los numeros primos existentes hasta el numero n
+	@param n: se obtiene los numeros primos menores a n
+	"""
+	lista = [];
+	if (n==1):
+		print "No hay numeros primos menor o igual que 1"
+	elif (n==2):
+		print "El unico numero primo es 2"
+	elif (n>2):
+		lista.append(2)
+		#Se utiliza la idea de que todo natural es compuesto de primos
+		#Se busca divisor de un numero n hasta sqrt(n)
+		for i in range(3,n+1):
+			j=0
+			primo = 1
+			while ((j<len(lista)) & (primo) & (lista[j]<=int(sqrt(i)))):
+				if (i%lista[j]==0):
+					primo = 0
+				j = j+1
+			if (primo):
+				lista.append(i)
+		print "Lista de numeros primos menor o igual a n:"
+		for k in range(len(lista)):
+			print lista[k]
+	else:
+		print "Se requiere un numero natural y sea distinto a 0"
+
+if (__name__ == "__main__"):
+	import sys
+	prim(int(sys.argv[1]))
+
+				 
